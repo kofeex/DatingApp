7281e933b22a533bffae37c2a769fb7a4adb153d
kofeex <speklja996@gmail.com>
2023-03-14 21:43:58 +0100

    Added messaging feature

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -11,12 +11,17 @@ namespace API.Helpers
         {   
             CreateMap<AppUser, MemberDto>()
                 .ForMember(dest => dest.PhotoUrl, 
-                    opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url))
+                    o => o.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url))
                 .ForMember(dest => dest.Age,
-                    opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+                    o => o.MapFrom(src => src.DateOfBirth.CalculateAge()));
             CreateMap<Photo, PhotoDto>();
             CreateMap<MemberUpdateDto, AppUser>();
             CreateMap<RegisterDto, AppUser>();
+            CreateMap<Message, MessageDto>()
+                .ForMember(d => d.SenderPhotoUrl,
+                     o => o.MapFrom(s => s.Sender.Photos.FirstOrDefault(x => x.IsMain).Url))
+                .ForMember(d => d.RecipientPhotoUrl,
+                     o => o.MapFrom(s => s.Recipient.Photos.FirstOrDefault(x => x.IsMain).Url));
         }
     }
 }
